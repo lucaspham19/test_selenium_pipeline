bee61b5b8afdc0f6d2aa03bd20e98f0422f37e5b
lucaspham19 <113858417+lucaspham19@users.noreply.github.com>
2024-03-23 13:11:29 -0400

    Update test_signin.py

--- tests/test_signin.py
+++ tests/test_signin.py
@@ -1,10 +1,11 @@
 import pytest
 
-from main import get_html_content
+from main import get_html_content, get_content_from_container
 
 class TestSample:
 
     def test_render_signin_screen_success(self):
-        content = get_html_content("https://focusearth.dev-16.toolbox.am/login")
-        # title = "Google"
-        # assert title == driver.title
+        soup = get_html_content("https://focusearth.dev-16.toolbox.am/login")
+        res = get_content_from_container(soup, "h2")
+        title = "Welcome to the Energy Portal"
+        assert title == res[0]
