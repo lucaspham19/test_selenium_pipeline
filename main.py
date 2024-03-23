547064a647bd45f24d32f37c16f6717ba7875cff
lucaspham19 <113858417+lucaspham19@users.noreply.github.com>
2024-03-23 13:08:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("Hello GH Action")
+import requests
+from bs4 import BeautifulSoup
+
+def get_html_content(URL: str):
+  page = requests.get(URL)
+  soup = BeautifulSoup(page.content, 'html.parser')
+
+  return soup
+
+def get_content(soup: BeautifulSoup, container: str):
+  return soup.find_all(container)
+
+
