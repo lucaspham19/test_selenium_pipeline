43e8044b3db41d3f0dd4c016fe27528c262603c4
lucaspham19 <113858417+lucaspham19@users.noreply.github.com>
2024-03-23 13:44:27 -0400

    Update test_signin.py

--- tests/test_signin.py
+++ tests/test_signin.py
@@ -1,8 +1,11 @@
 import pytest
 
-from main import get_html_content, get_content_from_container
+from main import get_html_content, get_content_from_container, initialize_driver
 
 class TestSample:
+
+    def __init__(self):
+        self.driver = initialize_driver()
 
     def test_render_signin_screen_success(self):
         soup = get_html_content("https://focusearth.dev-16.toolbox.am/login")
